0fb87f6a560e2ad8bf26637efaab2fc9f068eebb
Hayden Wang <haydenwang@ecoworkinc.com>
2018-10-15 00:10:16 -0700

    fix upper and lower bound of light sensor value

--- sketch_oct14a.ino
+++ sketch_oct14a.ino
@@ -1,19 +1,24 @@
 #include <Servo.h>
 Servo myservo;
+const int PIN_SERVO = 3;
+const int PIN_LIGHT = 0;
+const unsigned long BAUD_RATE = 9600;
+const unsigned long PERIOD_1SEC = 1000;
+const unsigned long PERIOD_1_5SEC = 1500;
+const unsigned long PERIOD_3SEC = 3000;
+const unsigned long PERIOD_5SEC = 5000;
+const unsigned long PERIOD_7SEC = 7000;
+const unsigned long PERIOD_10SEC = 10000;
+const unsigned long PERIOD_15SEC = 15000;
+
 unsigned long currentMilli = 0;
 unsigned long previousMilli1 = 0;
+unsigned long previousMilli1_5 = 0;
 unsigned long previousMilli3 = 0;
 unsigned long previousMilli5 = 0;
 unsigned long previousMilli7 = 0;
 unsigned long previousMilli10 = 0;
 unsigned long previousMilli15 = 0;
-
-const unsigned long period_1sec = 1000;
-const unsigned long period_3sec = 3000;
-const unsigned long period_5sec = 5000;
-const unsigned long period_7sec = 7000;
-const unsigned long period_10sec = 10000;
-const unsigned long period_15sec = 15000;
 
 int led_blink = 0;
 int led_status = 0;
@@ -26,50 +31,61 @@ unsigned long light_val_max = 0;
 unsigned long light_val_min = 0;
 
 void setup() {
-  Serial.begin(9600);
-  myservo.attach(3);
+  Serial.begin(BAUD_RATE);
+  myservo.attach(PIN_SERVO);
   pinMode(LED_BUILTIN, OUTPUT);
   if(light_calibration == 1) {
     Serial.print("Sart light calibration for 10 sec...\n");
     led_blink = 1;
+    light_value = analogRead(PIN_LIGHT);
+    light_val_min = light_value;
   }
 }
 
 void loop() {
   currentMilli = millis();
-  light_value = analogRead(0);
+  light_value = analogRead(PIN_LIGHT);
 
   // Light sensor calibration
-  if(light_calibration == 1) {
-    if(light_val_max < light_value) {
-      light_val_max = light_value;
-    }
-    if(light_val_min > light_value) {
-      light_val_min = light_value;
-    }
-  }
-  if(currentMilli - previousMilli10 > period_10sec) {
+  if(currentMilli - previousMilli10 > PERIOD_10SEC) {
     previousMilli10 = currentMilli;
     if(light_calibration == 1) {
       light_calibration = 0;
       led_blink = 0;
       Serial.print("Stop light calibration\n");
-      Serial.print("Result:\n");
       Serial.print("Maximum_light value: ");
       Serial.print(light_val_max);
-      Serial.print(", ");
+      Serial.print("\n");
       Serial.print("Minimun light value : ");
       Serial.print(light_val_min);
-      Serial.print("\n");
+      Serial.print("\n\n");
     }
   }
   
   // blinking or rotate servo motor
-  if(currentMilli - previousMilli1 > period_1sec) {
+  if(currentMilli - previousMilli1 > PERIOD_1SEC) {
     previousMilli1 = currentMilli;
+
+    if(light_calibration == 1) {
+      if(light_val_max < light_value) {
+        light_val_max = light_value;
+      }
+      if(light_val_min > light_value) {
+        light_val_min = light_value;
+      }
+    }
     
     if(led_blink == 1) {
       Serial.print(--calibrarion_time_counter);
+      
+      // for debug:
+      // Serial.print(" ");
+      // Serial.print(light_value);
+      // Serial.print(" ");
+      // Serial.print(light_val_max);
+      // Serial.print(" ");
+      // Serial.print(light_val_min);
+
       Serial.print(" sec...\n");
       if(led_status == 0) {
         digitalWrite(LED_BUILTIN, HIGH);
@@ -112,30 +128,30 @@ void loop() {
     previousMilli15 = 0;
   }
   
-  if(currentMilli - previousMilli3 >= period_3sec) {
+  if(currentMilli - previousMilli3 >= PERIOD_3SEC) {
     previousMilli3 = currentMilli;
-//    Serial.print("It's 3 seconds");
-//    Serial.print(" (current milli: ");
-//    Serial.print(previousMilli3);
-//    Serial.print(")");
-//    Serial.print("\n");
+  //  Serial.print("It's 3 seconds");
+  //  Serial.print(" (current milli: ");
+  //  Serial.print(previousMilli3);
+  //  Serial.print(")");
+  //  Serial.print("\n");
   }
   
-  if(currentMilli - previousMilli7 >= period_7sec) {
+  if(currentMilli - previousMilli7 >= PERIOD_7SEC) {
     previousMilli7 = currentMilli;
-//    Serial.print("It's 7 seconds");
-//    Serial.print(" (current milli: ");
-//    Serial.print(previousMilli7);
-//    Serial.print(")");
-//    Serial.print("\n");
+  //  Serial.print("It's 7 seconds");
+  //  Serial.print(" (current milli: ");
+  //  Serial.print(previousMilli7);
+  //  Serial.print(")");
+  //  Serial.print("\n");
   }
   
-  if(currentMilli - previousMilli15 >= period_15sec) {
+  if(currentMilli - previousMilli15 >= PERIOD_15SEC) {
     previousMilli15 = currentMilli;
-//    Serial.print("It's 15 seconds");
-//    Serial.print(" (current milli: ");
-//    Serial.print(previousMilli15);
-//    Serial.print(")");
-//    Serial.print("\n");
+  //  Serial.print("It's 15 seconds");
+  //  Serial.print(" (current milli: ");
+  //  Serial.print(previousMilli15);
+  //  Serial.print(")");
+  //  Serial.print("\n");
   }
 }
